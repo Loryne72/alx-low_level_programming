f4bff6047e813de407f03198e80217a506da3b1d
Loryne72 <lorinejemutai@gmail.com>
2023-06-17 19:19:46 +0300

    0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print a lin using the pintf function
+ * Return: 0(success)
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n",sizeof(char) );
+printf("Size of a int: %d byte(s)\n",sizeof(int) );
+
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+return (0);
+}
