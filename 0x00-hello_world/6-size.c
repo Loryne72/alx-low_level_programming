12915c342d4b3d91e94b2da9c00c2cd17076b8a1
Loryne72 <lorinejemutai@gmail.com>
2023-06-19 14:43:26 +0300

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 
 /**
  * main - A program that print a lin using the pintf function
- * Return: 0(success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-printf("Size of char: %d byte(s)\n",sizeof(char));
-printf("Size of int: %d byte(s)\n",sizeof(int));
-printf("Size of long int: %d bytes(s)\n",sizeof(long int));
-printf("Size of long long int: %d bytes(s)\n",sizeof(long long int));
-printf("size of float: %d bytes(s)\n",sizeof(float));
+printf("Size of a char: %d byte(s)\n",sizeof(char));
+printf("Size of an int: %d byte(s)\n",sizeof(int));
+printf("Size of a long int: %d bytes(s)\n",sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n",sizeof(long long int));
+printf("size of a float: %d bytes(s)\n",sizeof(float));
 return (0);
 }
